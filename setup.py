1acaa5cd587d5beda9be5ca9c72ef7c5e34a7a7b
David Loiseaux <david.loiseaux@inria.fr>
2023-12-22 23:03:20 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -113,7 +113,7 @@ setup(
     url="https://github.com/DavidLapous/multipers",
     # long_description=long_description,
     # long_description_content_type='text/markdown'
-    version="1.1.1",
+    version="1.1.2",
     license="MIT",
     keywords="TDA Persistence Multiparameter sklearn",
     ext_modules=cythonize(
