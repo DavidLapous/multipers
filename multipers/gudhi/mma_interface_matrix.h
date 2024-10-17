2af9d9542cd225dd447b24e9a03623a7d407590c
David Loiseaux <david.loiseaux@inria.fr>
2024-10-17 10:48:29 +0200

    fix: warning

--- multipers/gudhi/mma_interface_matrix.h
+++ multipers/gudhi/mma_interface_matrix.h
@@ -28,34 +28,27 @@
 
 namespace Gudhi::multiparameter::interface {
 
-template <Gudhi::persistence_matrix::Column_types column_type =
-              Gudhi::persistence_matrix::Column_types::INTRUSIVE_SET>
-struct Multi_persistence_options
-    : Gudhi::persistence_matrix::Default_options<column_type, true> {
+template <Gudhi::persistence_matrix::Column_types column_type = Gudhi::persistence_matrix::Column_types::INTRUSIVE_SET>
+struct Multi_persistence_options : Gudhi::persistence_matrix::Default_options<column_type, true> {
   static const bool has_matrix_maximal_dimension_access = false;
   static const bool has_column_pairings = true;
   static const bool has_vine_update = true;
   static const bool can_retrieve_representative_cycles = true;
 };
 
-template <Gudhi::persistence_matrix::Column_types column_type =
-              Gudhi::persistence_matrix::Column_types::INTRUSIVE_SET>
-struct Multi_persistence_Clement_options
-    : Gudhi::persistence_matrix::Default_options<column_type, true> {
+template <Gudhi::persistence_matrix::Column_types column_type = Gudhi::persistence_matrix::Column_types::INTRUSIVE_SET>
+struct Multi_persistence_Clement_options : Gudhi::persistence_matrix::Default_options<column_type, true> {
   static const bool has_matrix_maximal_dimension_access = false;
   static const bool has_column_pairings = true;
   static const bool has_vine_update = true;
   static const bool is_of_boundary_type = false;
-  static const Gudhi::persistence_matrix::Column_indexation_types
-      column_indexation_type =
-          Gudhi::persistence_matrix::Column_indexation_types::POSITION;
+  static const Gudhi::persistence_matrix::Column_indexation_types column_indexation_type =
+      Gudhi::persistence_matrix::Column_indexation_types::POSITION;
   static const bool can_retrieve_representative_cycles = true;
 };
 
-template <Gudhi::persistence_matrix::Column_types column_type =
-              Gudhi::persistence_matrix::Column_types::INTRUSIVE_SET>
-struct No_vine_multi_persistence_options
-    : Gudhi::persistence_matrix::Default_options<column_type, true> {
+template <Gudhi::persistence_matrix::Column_types column_type = Gudhi::persistence_matrix::Column_types::INTRUSIVE_SET>
+struct No_vine_multi_persistence_options : Gudhi::persistence_matrix::Default_options<column_type, true> {
   static const bool has_matrix_maximal_dimension_access = false;
   static const bool has_column_pairings = true;
   static const bool has_vine_update = false;
@@ -63,8 +56,7 @@ struct No_vine_multi_persistence_options
 
 template <class Matrix_options, class Boundary_matrix_type>
 class Persistence_backend_matrix {
-
-public:
+ public:
   using matrix_type = Gudhi::persistence_matrix::Matrix<Matrix_options>;
   using cycle_type = typename matrix_type::Cycle;
   static const bool is_vine = Matrix_options::has_vine_update;
@@ -76,27 +68,21 @@ public:
   using Index = typename matrix_type::Index;
   using dimension_type = typename matrix_type::Dimension;
 
-  class Barcode_iterator
-      : public boost::iterator_facade<Barcode_iterator, const bar &,
-                                      boost::forward_traversal_tag> {
-  public:
-    Barcode_iterator(const typename matrix_type::Barcode *barcode,
-                     const std::vector<std::size_t> *inv)
-        : barcode_(barcode->size() == 0 ? nullptr : barcode),
-          perm_(barcode->size() == 0 ? nullptr : inv), currPos_(0) {
+  class Barcode_iterator : public boost::iterator_facade<Barcode_iterator, const bar &, boost::forward_traversal_tag> {
+   public:
+    Barcode_iterator(const typename matrix_type::Barcode *barcode, const std::vector<std::size_t> *inv)
+        : barcode_(barcode->size() == 0 ? nullptr : barcode), perm_(barcode->size() == 0 ? nullptr : inv), currPos_(0) {
       if (barcode_ != nullptr && perm_ != nullptr) {
         auto &b = barcode_->operator[](currPos_);
         currBar_.dim = b.dim;
         currBar_.birth = perm_->operator[](b.birth);
-        currBar_.death = b.death == static_cast<pos_index>(-1)
-                             ? -1
-                             : perm_->operator[](b.death);
+        currBar_.death = b.death == static_cast<pos_index>(-1) ? -1 : perm_->operator[](b.death);
       }
     }
 
     Barcode_iterator() : barcode_(nullptr), perm_(nullptr), currPos_(0) {}
 
-  private:
+   private:
     // mandatory for the boost::iterator_facade inheritance.
     friend class boost::iterator_core_access;
 
@@ -106,8 +92,7 @@ public:
     bar currBar_;
 
     bool equal(Barcode_iterator const &other) const {
-      return barcode_ == other.barcode_ && perm_ == other.perm_ &&
-             currPos_ == other.currPos_;
+      return barcode_ == other.barcode_ && perm_ == other.perm_ && currPos_ == other.currPos_;
     }
 
     const bar &dereference() const { return currBar_; }
@@ -123,36 +108,32 @@ public:
         auto &b = barcode_->operator[](currPos_);
         currBar_.dim = b.dim;
         currBar_.birth = perm_->operator[](b.birth);
-        if (debug &&
-            currBar_.birth >
-                std::numeric_limits<decltype(currBar_.birth)>::max() / 2) {
+        if (debug && currBar_.birth > std::numeric_limits<decltype(currBar_.birth)>::max() / 2) {
           std::cout << currBar_ << std::endl;
           std::cout << "while " << b.birth;
           std::cout << "  " << perm_->size();
         }
-        currBar_.death = b.death == static_cast<pos_index>(-1)
-                             ? -1
-                             : perm_->operator[](b.death);
+        currBar_.death = b.death == static_cast<pos_index>(-1) ? -1 : perm_->operator[](b.death);
       }
     }
   };
+
   class Barcode {
-  public:
+   public:
     using iterator = Barcode_iterator;
-    Barcode(matrix_type &matrix, const std::vector<std::size_t> *perm)
-        : barcode_(&matrix.get_current_barcode()) {
+
+    Barcode(matrix_type &matrix, const std::vector<std::size_t> *perm) : barcode_(&matrix.get_current_barcode()) {
       const bool debug = false;
       if constexpr (Matrix_options::has_vine_update) {
         perm_ = perm;
       } else {
         perm_.reserve(perm->size());
         for (const auto &stuff : *perm)
-          if (stuff < perm->size()) // number of generators
+          if (stuff < perm->size())  // number of generators
             perm_.push_back(stuff);
       }
       if constexpr (debug) {
-        std::cout << "Built matrix of size " << matrix.get_number_of_columns()
-                  << " / " << perm->size() << std::endl;
+        std::cout << "Built matrix of size " << matrix.get_number_of_columns() << " / " << perm->size() << std::endl;
       }
     }
 
@@ -172,8 +153,7 @@ public:
     /* } */
     std::size_t size() const { return barcode_->size(); }
 
-    inline friend std::ostream &operator<<(std::ostream &stream,
-                                           Barcode &structure) {
+    inline friend std::ostream &operator<<(std::ostream &stream, Barcode &structure) {
       stream << "Barcode: " << structure.size() << "\n";
       for (const auto bar : structure) {
         stream << "[" << bar.dim << "] ";
@@ -184,21 +164,19 @@ public:
       return stream;
     }
 
-  private:
+   private:
     const typename matrix_type::Barcode *barcode_;
     typename std::conditional<Matrix_options::has_vine_update,
                               const std::vector<std::size_t> *,
                               std::vector<std::size_t>>::type perm_;
   };
 
-  Persistence_backend_matrix() : permutation_(nullptr){};
-  Persistence_backend_matrix(const Boundary_matrix_type &boundaries,
-                             std::vector<std::size_t> &permutation)
-      : matrix_(boundaries.size()), permutation_(&permutation) {
+  Persistence_backend_matrix() : permutation_(nullptr) {};
 
+  Persistence_backend_matrix(const Boundary_matrix_type &boundaries, std::vector<std::size_t> &permutation)
+      : matrix_(boundaries.size()), permutation_(&permutation) {
     const bool verbose = false;
-    if constexpr (verbose)
-      std::cout << "Constructing matrix..." << std::endl;
+    if constexpr (verbose) std::cout << "Constructing matrix..." << std::endl;
     std::vector<std::size_t> permutationInv(permutation_->size());
     std::vector<std::size_t> boundary_container;
     std::size_t c = 0;
@@ -210,23 +188,22 @@ public:
       permutationInv[i] = c++;
       boundary_container.resize(boundaries[i].size());
       if constexpr (verbose)
-        std::cout << i << "/" << permutation_->size() << " c= " << c
-                  << " dimension " << boundaries.dimension(i) << "..."
-                  << std::endl
+        std::cout << i << "/" << permutation_->size() << " c= " << c << " dimension " << boundaries.dimension(i)
+                  << "..." << std::endl
                   << std::flush;
       for (std::size_t j = 0; j < boundaries[i].size(); ++j) {
         boundary_container[j] = permutationInv[boundaries[i][j]];
       }
       std::sort(boundary_container.begin(), boundary_container.end());
-      matrix_.insert_boundary(c - 1, boundary_container,
-                              boundaries.dimension(i));
+      matrix_.insert_boundary(c - 1, boundary_container, boundaries.dimension(i));
     }
   }
+
   Persistence_backend_matrix(const Persistence_backend_matrix &toCopy)
       : matrix_(toCopy.matrix_), permutation_(toCopy.permutation_) {}
+
   Persistence_backend_matrix(Persistence_backend_matrix &&other) noexcept
-      : matrix_(std::move(other.matrix_)),
-        permutation_(std::exchange(other.permutation_, nullptr)) {}
+      : matrix_(std::move(other.matrix_)), permutation_(std::exchange(other.permutation_, nullptr)) {}
 
   Persistence_backend_matrix &operator=(Persistence_backend_matrix other) {
     swap(matrix_, other.matrix_);
@@ -234,28 +211,24 @@ public:
     return *this;
   }
 
-  friend void swap(Persistence_backend_matrix &be1,
-                   Persistence_backend_matrix &be2) {
+  friend void swap(Persistence_backend_matrix &be1, Persistence_backend_matrix &be2) {
     swap(be1.matrix_, be2.matrix_);
     std::swap(be1.permutation_, be2.permutation_);
   }
 
-  inline dimension_type get_dimension(pos_index i) {
-    return matrix_.get_column_dimension(i);
-  }
+  inline dimension_type get_dimension(pos_index i) { return matrix_.get_column_dimension(i); }
 
   inline void vine_swap(pos_index i) { matrix_.vine_swap(i); }
 
   inline Barcode get_barcode() { return Barcode(matrix_, permutation_); }
+
   inline std::size_t size() { return this->matrix_.get_number_of_columns(); }
 
-  inline friend std::ostream &
-  operator<<(std::ostream &stream, Persistence_backend_matrix &structure) {
+  inline friend std::ostream &operator<<(std::ostream &stream, Persistence_backend_matrix &structure) {
     stream << "[\n";
     for (auto i = 0u; i < structure.matrix_.get_number_of_columns(); i++) {
       stream << "[";
-      for (const auto &stuff : structure.matrix_.get_column(i))
-        stream << stuff << ", ";
+      for (const auto &stuff : structure.matrix_.get_column(i)) stream << stuff << ", ";
       stream << "]\n";
     }
 
@@ -281,7 +254,6 @@ public:
       matrix_.update_representative_cycles();
     std::vector<std::vector<std::vector<unsigned int>>> current_cycles =
         matrix_.get_representative_cycles_as_borders(detailed);
-    unsigned int i = 0;
     for (auto &cycle : current_cycles) {
       if constexpr (verbose) std::cout << "Cycle (matrix_ order): ";
       for (auto &border : cycle) {
@@ -289,19 +261,16 @@ public:
           b = permutation2[b];
         }
       }
-      ++i;
     }
     return current_cycles;
   }
 
-  inline void _update_permutation_ptr(std::vector<std::size_t> &perm) {
-    permutation_ = &perm;
-  }
+  inline void _update_permutation_ptr(std::vector<std::size_t> &perm) { permutation_ = &perm; }
 
-private:
+ private:
   matrix_type matrix_;
   std::vector<std::size_t> *permutation_;
 };
 
-} // namespace Gudhi::multiparameter::interface
-#endif // MMA_INTERFACE_MATRIX_H
+}  // namespace Gudhi::multiparameter::interface
+#endif  // MMA_INTERFACE_MATRIX_H
