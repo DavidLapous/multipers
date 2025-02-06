2ca44cb1b58039cb7bbdcb41f770d82e8e9480f1
David Loiseaux <david.loiseaux@inria.fr>
2025-02-06 14:56:03 +0100

    optim: rips

--- multipers/torch/rips_density.py
+++ multipers/torch/rips_density.py
@@ -68,9 +68,12 @@ def function_rips_signed_measure_old(
     if threshold < np.inf:
         distance_matrix[distance_matrix > threshold] = np.inf
 
-    st = RipsComplex(
-        distance_matrix=distance_matrix.detach(), max_edge_length=threshold
-    ).create_simplex_tree()
+    # st = RipsComplex(
+    #     distance_matrix=distance_matrix.detach(), max_edge_length=threshold
+    # ).create_simplex_tree()
+    st = gd.SimplexTree.create_from_array(
+        distance_matrix.detach(), max_filtration=threshold
+    )
     # detach makes a new (reference) tensor, without tracking the gradient
     st = mp.SimplexTreeMulti(st, num_parameters=2, safe_conversion=safe_conversion)
     st.fill_lowerstar(
