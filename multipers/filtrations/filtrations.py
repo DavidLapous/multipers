fbe857ad1f629793a0e830144854e3f3ea7e15f9
David Loiseaux <david.loiseaux@inria.fr>
2025-07-23 11:21:45 +0900

    fix: tests

--- multipers/filtrations/filtrations.py
+++ multipers/filtrations/filtrations.py
@@ -17,7 +17,6 @@ try:
 
     from multipers.filtrations.density import KDE
 except ImportError:
-
     from sklearn.neighbors import KernelDensity
 
     warn("pykeops not found. Falling back to sklearn.")
@@ -43,9 +42,9 @@ def RipsLowerstar(
      - function : ArrayLike of shape (num_data, num_parameters -1)
      - threshold_radius:  max edge length of the rips. Defaults at min(max(distance_matrix, axis=1)).
     """
-    assert (
-        points is not None or distance_matrix is not None
-    ), "`points` or `distance_matrix` has to be given."
+    assert points is not None or distance_matrix is not None, (
+        "`points` or `distance_matrix` has to be given."
+    )
     if distance_matrix is None:
         api = api_from_tensor(points)
         points = api.astensor(points)
@@ -67,7 +66,8 @@ def RipsLowerstar(
         function = function[:, None]
     if function.ndim != 2:
         raise ValueError(
-            f"`function.ndim` should be 0 or 1 . Got {function.ndim=}.{function=}"
+            f"`function.ndim` should be 0 or 1 . Got {
+                function.ndim=}.{function=}"
         )
     num_parameters = function.shape[1] + 1
     st = SimplexTreeMulti(st, num_parameters=num_parameters)
@@ -93,9 +93,9 @@ def RipsCodensity(
     """
     Computes the Rips density filtration.
     """
-    assert (
-        bandwidth is None or dtm_mass is None
-    ), "Density estimation is either via kernels or dtm."
+    assert bandwidth is None or dtm_mass is None, (
+        "Density estimation is either via kernels or dtm."
+    )
     if bandwidth is not None:
         kde = KDE(bandwidth=bandwidth, kernel=kernel, return_log=return_log)
         f = -kde.fit(points).score_samples(points)
@@ -142,9 +142,9 @@ def DelaunayLowerstar(
         warn("Cannot keep points gradient unless using `flagify=True`.")
     points = api.astensor(points)
     function = api.astensor(function).squeeze()
-    assert (
-        function.ndim == 1
-    ), "Delaunay Lowerstar is only compatible with 1 additional parameter."
+    assert function.ndim == 1, (
+        "Delaunay Lowerstar is only compatible with 1 additional parameter."
+    )
     slicer = from_function_delaunay(
         api.asnumpy(points),
         api.asnumpy(function),
@@ -154,6 +154,9 @@ def DelaunayLowerstar(
         verbose=verbose,
         clear=clear,
     )
+    if reduce_degree >= 0:
+        # Force resolution to avoid confusion with hilbert.
+        slicer = slicer.minpres(degree=reduce_degree, force=True)
     if flagify:
         from multipers.slicer import to_simplextree
 
@@ -187,9 +190,9 @@ def DelaunayCodensity(
     """
     TODO
     """
-    assert (
-        bandwidth is None or dtm_mass is None
-    ), "Density estimation is either via kernels or dtm."
+    assert bandwidth is None or dtm_mass is None, (
+        "Density estimation is either via kernels or dtm."
+    )
     if bandwidth is not None:
         kde = KDE(bandwidth=bandwidth, kernel=kernel, return_log=return_log)
         f = -kde.fit(points).score_samples(points)
@@ -277,9 +280,9 @@ def CoreDelaunay(
 
     assert len(ks) > 0, "The parameter ks must contain at least one value."
     assert np.all(ks > 0), "All values in ks must be positive."
-    assert np.all(
-        ks <= len(points)
-    ), "All values in ks must be less than or equal to the number of points in the point cloud."
+    assert np.all(ks <= len(points)), (
+        "All values in ks must be less than or equal to the number of points in the point cloud."
+    )
     assert len(points) > 0, "The point cloud must contain at least one point."
     assert points.ndim == 2, f"The point cloud must be a 2D array, got {points.ndim}D."
     assert beta >= 0, f"The parameter beta must be positive, got {beta}."
@@ -287,11 +290,15 @@ def CoreDelaunay(
         "safe",
         "exact",
         "fast",
-    ], f"The parameter precision must be one of ['safe', 'exact', 'fast'], got {precision}."
+    ], (
+        f"The parameter precision must be one of ['safe', 'exact', 'fast'], got {precision}."
+    )
 
     if verbose:
         print(
-            f"Computing the Delaunay Core Bifiltration of {len(points)} points in dimension {points.shape[1]} with parameters:"
+            f"Computing the Delaunay Core Bifiltration of {
+                len(points)
+            } points in dimension {points.shape[1]} with parameters:"
         )
         print(f"\tbeta = {beta}")
         print(f"\tks = {ks}")
@@ -333,7 +340,9 @@ def CoreDelaunay(
         num_simplices = len(vertex_array)
         if verbose:
             print(
-                f"Inserting {num_simplices} simplices of dimension {dim} ({num_simplices * len(ks)} birth values)..."
+                f"Inserting {num_simplices} simplices of dimension {dim} ({
+                    num_simplices * len(ks)
+                } birth values)..."
             )
         max_knn_distances = np.max(knn_distances[vertex_array], axis=1)
         critical_radii = np.maximum(alphas[:, None], beta * max_knn_distances)
