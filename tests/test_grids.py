1ed820a168cf09347257b8801acaf98506097a3b
David Loiseaux <david.loiseaux@inria.fr>
2025-08-12 14:48:51 +0200

    tests: more

--- tests/test_grids.py
+++ tests/test_grids.py
@@ -1,8 +1,8 @@
 import numpy as np
 import pytest
 
-import multipers.grids as mpg
 import multipers as mp
+import multipers.grids as mpg
 
 try:
     import torch
@@ -57,8 +57,8 @@ def test_regular_left():
         y,
         [
             0.0,
-            .1,
-            .21,
+            0.1,
+            0.21,
             1,
         ],
     ).all(), y
@@ -68,9 +68,38 @@ def test_regular_left():
         y_torch = torchapi.from_numpy(y)
         assert torch.allclose(
             y_torch,
-            torch.tensor(
-                [0.0, 0.1, 0.21, 1.0], dtype=torch.float32
-            ),
+            torch.tensor([0.0, 0.1, 0.21, 1.0], dtype=torch.float32),
         ), y_torch
     else:
         pytest.skip("Skipping test as torch is not available.")
+
+
+def test_sanity_numpy():
+    for k in range(2, 4):
+        img = np.random.random(size=(50, 50, k))
+        s = mp.filtrations.Cubical(img)
+        for strat in mp.grids.available_strategies:
+            _s = s.grid_squeeze(
+                strategy=strat, resolution=None if strat == "exact" else 10
+            )
+            f = _s.filtration_grid[0]
+            if strat not in ["exact", "precomputed"]:
+                assert len(f) <= 10, f"invalid resolution for {strat=}"
+
+
+@pytest.mark.skipif(torch is None, reason="Torch not installed.")
+def test_sanity_torch():
+    import torch
+
+    for k in range(2, 4):
+        img = torch.rand(size=(50, 50, k)).requires_grad_()
+        s = mp.filtrations.Cubical(img)
+        for strat in mp.grids.available_strategies:
+            _s = s.grid_squeeze(
+                strategy=strat, resolution=None if strat == "exact" else 10
+            )
+            f = _s.filtration_grid[0]
+            if strat not in ["regular", "partition"]:
+                assert f.requires_grad, f"Grad not working for {strat=}"
+            if strat not in ["exact", "precomputed"]:
+                assert len(f) <= 10, f"invalid resolution for {strat=}"
