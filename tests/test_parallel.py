ad9d60ce70639c155388dc7242f970a8b0795969
David Loiseaux <david.loiseaux@inria.fr>
2025-12-20 17:24:32 +0100

    .

--- tests/test_parallel.py
+++ tests/test_parallel.py
@@ -24,7 +24,7 @@ def io_fd_mpfree2(x):
     not (mio._check_available("mpfree") and mio._check_available("function_delaunay")),
     reason="Skipped external test as `function_delaunay`, `mpfree` were not found.",
 )
-@pytest.mark.flaky(reruns=2)
+@pytest.mark.flaky(reruns=3)
 @pytest.mark.parametrize("backend", ["loky", "threading"])
 def test_io_parallel(backend):
     x = mp.data.three_annulus(100, 50)
@@ -51,13 +51,13 @@ def get_sm_st(n_jobs=1, to_slicer=False, invariant="hilbert"):
     return mp.signed_measure(st, degree=1, n_jobs=n_jobs, invariant=invariant)[0]
 
 
-@pytest.mark.flaky(reruns=2)
+@pytest.mark.flaky(reruns=3)
 @pytest.mark.parametrize("backend", ["loky", "threading"])
 @pytest.mark.parametrize("slicer", [False, True])
 @pytest.mark.parametrize("invariant", ["hilbert"])
 def test_st_sm_parallel(backend, slicer, invariant):
-    ground_truth = get_sm_st(n_jobs=1, to_slicer=slicer)
-    ground_truth2 = get_sm_st(n_jobs=1, to_slicer=not slicer)
+    ground_truth = get_sm_st(n_jobs=1, to_slicer=slicer, invariant=invariant)
+    ground_truth2 = get_sm_st(n_jobs=1, to_slicer=not slicer, invariant=invariant)
     assert_sm_pair(ground_truth, ground_truth2, reg=0, exact=False, max_error=1e-12)
     sms = Parallel(n_jobs=-1, backend=backend)(
         delayed(get_sm_st)(-1, slicer, invariant) for _ in range(15)
