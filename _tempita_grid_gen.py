7eea0858a924e2f5e5e48916481ea6e69fbfd7c0
David Loiseaux <david.loiseaux@inria.fr>
2026-01-20 18:55:02 +0100

    fix: bad blacklist

--- _tempita_grid_gen.py
+++ _tempita_grid_gen.py
@@ -88,10 +88,10 @@ def check_combination(
     if backend_type in ["Graph", "GudhiCohomology"]:
         if column_type[0] != 0:
             return False
-    if filtration_container == "flat":
+    if filtration_container == "Degree_rips_bifiltration":
         if not is_kcritical:
             return False
-        if value_type[0] == "f":
+        if value_type[-1][0] == "f":
             return False
     return True
 
