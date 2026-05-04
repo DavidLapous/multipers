9abec461079e7b5173a43b75f474b213d74c0275
David Loiseaux <david.loiseaux@inria.fr>
2026-05-04 13:22:42 -0700

    fix : doc build

--- docs/build_doc.sh
+++ docs/build_doc.sh
@@ -1,4 +1,7 @@
-rm -rf source 
+#!/usr/bin/env sh
+set -eu
+
+rm -rf source
 sphinx-apidoc -f -P -o source ../multipers
-rm -rf _build 
-make html
+rm -rf _build
+sphinx-build -M html . _build ${SPHINXOPTS:-} ${O:-}
